ad91872f9d509ed7d0a183039cc6785a4c152ea9
alfazsozib <alfazsozib@gmail.com>
2023-05-23 03:43:45 +0600

    admin page added

---
+++ app/admin/page.jsx
@@ -0,0 +1,212 @@
+import React from 'react'
+
+function Admin() {
+  return (
+    <div className="admin-panel">
+      <div className='w-[100%]'>
+        <h1 className='text-center'>Admin Dasboard</h1>
+      </div>
+      <div >
+        <form action="">
+          <div className='grid grid-cols-4 p-4 gap-4'>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div className="relative z-0 w-full mb-6 group">
+              <input
+                type="number"
+                name="publicMintPrice"
+                className="placeholder-transparent focus:placeholder-gray-500 block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-[#cccccc] appearance-none dark:text-white  dark:focus:border-[#FCC607] focus:outline-none focus:ring-0 focus:border-[#FCC607] peer"
+                placeholder="0.1.."
+              />
+              <label
+                for="floating_first_name"
+                className="peer-focus:font-medium absolute text-base text-[#cccccc]  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#FCC607] peer-focus:dark:text-[#FCC607] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Public Mint Price
+              </label>
+            </div>
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-white dark:text-white"
+                for="file_input"
+              >
+                Upload Image or Video(.mp4)
+              </label>
+              <input
+                className="placeholder-transparent focus:placeholder-gray-500 block w-full text-base text-[#cccccc] border border-[#cccccc] rounded-lg cursor-pointer  focus:outline-none"
+                id="file_input"
+                type="file"
+                name='image'
+
+                accept="image/*,video/*"
+              />
+            </div>
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-white dark:text-white"
+                for="file_input"
+              >
+                Upload Image or Video(.mp4)
+              </label>
+              <input
+                className="placeholder-transparent focus:placeholder-gray-500 block w-full text-base text-[#cccccc] border border-[#cccccc] rounded-lg cursor-pointer  focus:outline-none"
+                id="file_input"
+                type="file"
+                name='image'
+
+                accept="image/*,video/*"
+              />
+            </div>
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-white dark:text-white"
+                for="file_input"
+              >
+                Upload Image or Video(.mp4)
+              </label>
+              <input
+                className="placeholder-transparent focus:placeholder-gray-500 block w-full text-base text-[#cccccc] border border-[#cccccc] rounded-lg cursor-pointer  focus:outline-none"
+                id="file_input"
+                type="file"
+                name='image'
+
+                accept="image/*,video/*"
+              />
+            </div>
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-white dark:text-white"
+                for="file_input"
+              >
+                Upload Image or Video(.mp4)
+              </label>
+              <input
+                className="placeholder-transparent focus:placeholder-gray-500 block w-full text-base text-[#cccccc] border border-[#cccccc] rounded-lg cursor-pointer  focus:outline-none"
+                id="file_input"
+                type="file"
+                name='image'
+                accept="image/*,video/*"
+              />
+            </div>
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-white dark:text-white"
+                for="file_input"
+              >
+                Upload Image or Video(.mp4)
+              </label>
+              <input
+                className="placeholder-transparent focus:placeholder-gray-500 block w-full text-base text-[#cccccc] border border-[#cccccc] rounded-lg cursor-pointer  focus:outline-none"
+                id="file_input"
+                type="file"
+                name='image'
+                accept="image/*,video/*"
+              />
+            </div>
+          
+          </div>
+        </form>
+      </div>
+      <div>
+      </div>
+    </div>
+  )
+}
+
+export default Admin
